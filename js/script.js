d0cbd106a9743868052ea8260b9890da6b67fd09
bahaeddine10 <75793407+bahaeddine10@users.noreply.github.com>
2024-09-13 13:38:15 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function startGame(){
     startBtn.classList.add('hide');
     const prog = document.createElement("div");
     prog.classList.add("progress-bar");
-    prog.setAttribute("data-label", "Loading");
+    prog.setAttribute("data-label", " ");
     container.appendChild(prog);
     const progressBar=document.getElementsByClassName("progress-bar")[0];
     setInterval(() => {
